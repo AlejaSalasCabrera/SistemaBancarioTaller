7590ddbfa8b0ee989da15279e34f7cbbad93f1ec
alejandra salas <aleja.20012010@gmail.com>
2018-08-28 13:35:59 -0500

    menu

--- SistemaBancario-master/Sistemas_Bancario.py
+++ SistemaBancario-master/Sistemas_Bancario.py
@@ -11,12 +11,12 @@ class Prestamo:
     num_cuo_pres = 0
 
     def Registrar_prestamo(self):
-        self.id_userp = input("digite la id de usuario: ")
+        self.id_userp = input("Digite la id de usuario: ")
         for iu in user:
             if iu['id'] == self.id_userp:
-                self.id_pres = input("digite la id prestamo: ")
-                self.cant_pres = int(input("Ingrese la cantidad del prestamo "))
-                self.num_cuo_pres = int(input("Ingrese numero de cuotas del prestamo "))
+                self.id_pres = input("Digite la id prestamo: ")
+                self.cant_pres = int(input("Ingrese la cantidad del prestamo: "))
+                self.num_cuo_pres = int(input("Ingrese numero de cuotas del prestamo: "))
                 pres.append({'id_user': self.id_userp, 'id_pres': self.id_pres, 'can_pres': self.cant_pres, 'num_cuo': self.num_cuo_pres})
                 nc = self.num_cuo_pres
                 if (nc > 0):
@@ -38,31 +38,38 @@ class Cuota:
                 print(iup['num_cuo'], iup['valor_cuo'])
         cp = int(input("Digite el numero de la cuota a pagar: "))
         for bc in cuo:
-            if bc['num_cuo'] == cp:
-                if bc['estado'] == False:
-                    bc['estado'] = True
+            if bc['id_user'] == self.id_userpc and bc['id_pres'] == self.id_presc:
+                if bc['num_cuo'] == cp:
+                    if bc['estado'] == False:
+                        bc['estado'] = True
 
         #Reportes
 
     def Cuotas_pagadas(self):
+        self.id_userpc = input("ingrese la id del usuario: ")
+        self.id_presc = input("ingrese la id del prestamo a pagar: ")
+        cpa = 0
         for c_pa in cuo:
-            if c_pa['estado'] == True:
-                print("Las cuotas pagadas son:{} {}".format(c_pa['num_cuo'], c_pa['valor_cuo']))
+            if c_pa['id_user'] == self.id_userpc and c_pa['id_pres'] == self.id_presc:
+                if c_pa['estado'] == True:
+                    cpa = cpa + 1
+        print("Las cuotas pagadas son {} por valor de {}".format(cpa, c_pa['valor_cuo']))
 
     def Numero_Cuota_Mora(self):
-
-
-
+        self.id_userpc = input("ingrese la id del usuario: ")
+        self.id_presc = input("ingrese la id del prestamo a pagar: ")
+        cm = 0
         for c_p_m in cuo:
-            if c_p_m['estado'] == False:
-                cm = cm+1
-                print("Las cuotas en mora son {} por valor de {} ".format(cm, c_p_m['valor_cuo']))
+            if c_p_m['id_user'] == self.id_userpc and c_p_m['id_pres'] == self.id_presc:
+                if c_p_m['estado'] == False:
+                    cm = cm+1
+        print("Las cuotas en mora son {} por valor de {} ".format(cm, c_p_m['valor_cuo']))
 
     def Todos_Prestamo(self):
         self.id_u=input("Digite su identificacion: ")
         for tp in pres:
             if tp['id_user'] == self.id_u:
-                print("La id del prestamos es {} por un valor de {} diferido a {} cuotas ".format(tp['id_pres'], tp['can_pres'], tp['num_cuo']))
+                print("La id del prestamo es {} por un valor de {} diferido a {} cuotas ".format(tp['id_pres'], tp['can_pres'], tp['num_cuo']))
 
 
 class User(Prestamo, Cuota):
@@ -81,7 +88,7 @@ print("2. Solicitar Prestamo")
 print("3. Pagar cuotas")
 print("4. Reportes")
 print("\n")
-opc =int(input("Digite la opcion: "))
+opc =int(input("Digite la opcion del MENU PRINCIPAL: "))
 
 
 while opc != 5:
@@ -89,43 +96,57 @@ while opc != 5:
 
     if opc == 1:
         usuario.Registro_User()
-        print(user)
+        #print(user)
+        print("\n")
 
     elif opc == 2:
         usuario.Registrar_prestamo()
-        print(pres)
-        print(cuo)
-
+        #print(pres)
+        #print(cuo)
+        print("\n")
     elif opc == 3:
         usuario.Mostar_Pagar_cuota()
-        print(cuo)
-
+        #print(cuo)
+        print("\n")
     elif opc == 4:
+        print("\n")
+        print("MENU DE REPORTES")
         print("1. Consultar numero de cuotas pagadas")
         print("2. Consultar numero de cuotas en mora")
         print("3. Consultar todos sus prestamos")
-
-        nopc = int(input("Digite la opcion para de reportes que desea optener: "))
+        print("4. Regresar al MENU PRINCIPAL")
+        print("\n")
+        nopc = int(input("Digite la opcion del MENU DE REPORTES: "))
         while nopc != 4:
 
             if nopc == 1:
                 usuario.Cuotas_pagadas()
-                print("1,1")
-
+                #print("1,1")
+                print("\n")
             elif nopc == 2:
                 usuario.Numero_Cuota_Mora()
-                print("1,2")
-
+                #print("1,2")
+                print("\n")
             elif nopc == 3:
                 usuario.Todos_Prestamo()
-                print("1,3")
-
+                #print("1,3")
+                print("\n")
             else:
                 print('La opcion digitada no existe')
 
-            nopc = int(input("\nDigite la opcion Nuevamente: "))
+            print("MENU DE REPORTES")
+            print("1. Consultar numero de cuotas pagadas")
+            print("2. Consultar numero de cuotas en mora")
+            print("3. Consultar todos sus prestamos")
+            print("\n")
+            nopc = int(input("Digite la opcion del MENU DE REPORTES: "))
 
     else:
         print('La opcion digitada no existe')
 
-    opc =int(input("\nDigite la opcion: "))
+    print("MENU DE OPCIONES")
+    print("1. Crear Usuario")
+    print("2. Solicitar Prestamo")
+    print("3. Pagar cuotas")
+    print("4. Reportes")
+    opc =int(input("\nDigite la opcion del MENU PRINCIPAL: "))
